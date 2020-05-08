85805111eb2002303891dc113555ecd9205a9011
sergioge99 <sergioge99@users.noreply.github.com>
2020-05-08 05:43:45 +0200

    bug

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Contacta from '../Home/Contacta.js';
 import './About.css';
 
 import logo from '../cte/pandora-texto.png';
@@ -12,19 +13,18 @@ class About extends React.Component {
         <div className="about">
           <div className="row">
             <div className="column col-1">
-            <img src={logo} alt="Pandora"/>
+              <img src={logo} alt="Pandora"/>
             </div>
             <div className="column col-2">
-            <h1>Pandora es una plataforma sin ánimo de lucro<br/>que vela por la seguridad de tus contraseñas,<br/>
-            mateniéndolas en secreto gracias a:<br/> AES-256, criptografía de clave pública, sal...<br/>
-            Entre muchas otras características<br/><br/>El equipo que está detrás de Pandora está<br/>
-            formado por ocho estudiantes del grado en<br/>Ingeniería Informática de la Universidad de Zaragoza</h1>
-            <button className="but">
-            <span class="fab fa-github"></span>
-            </button>
-            </div>
+              <h1>Pandora es una plataforma sin ánimo de lucro<br/>que vela por la seguridad de tus contraseñas,<br/>
+              mateniéndolas en secreto gracias a:<br/> AES-256, criptografía de clave pública, sal...<br/>
+              Entre muchas otras características<br/><br/>El equipo que está detrás de Pandora está<br/>
+              formado por ocho estudiantes del grado en<br/>Ingeniería Informática de la Universidad de Zaragoza</h1>
+              </div>
           </div>
-
+          <div className="row">
+            <Contacta/>
+          </div>
         </div>
       </div>
     );
